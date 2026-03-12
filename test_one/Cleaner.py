a3d903c7bd7e462bb5010625f49804d0a69f7fa6
Rodrigo Zavala <rodrigoztrejo@gmail.com>
2026-03-12 15:09:47 -0600

    [added] functionality to clean text data

---
+++ test_one/Cleaner.py
@@ -0,0 +1,12 @@
+class Cleaner:
+    """
+        Class to clean the input data, removing punctuation and converting to lowercase.
+    """
+    def __init__(self, input_data : str):
+        self.input_data = input_data.split()
+
+    def clean(self) -> list[str]:
+        # Cleaning data by removing punctuation and converting to lowercase
+        cleaned_data = [item.strip('.').strip(',').lower() for item in self.input_data]
+
+        return cleaned_data
