45c887e80b059120b17736ec89d17d245878a7a1
Rodrigo Zavala <rodrigoztrejo@gmail.com>
2026-03-12 20:41:44 -0600

    [added] execution file created including interative menu

---
+++ test_three/execution.py
@@ -0,0 +1,70 @@
+from ExcelSheet import ExcelSheet
+
+import sys
+import os
+
+def display_menu():
+    # Clears the terminal screen for a cleaner look (os.system works on most systems)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("="*20)
+    print("MENU Excel")
+    print("="*20)
+    print("1. Insertar Informacion Celda")
+    print("2. Actualizar Informacion Celda")
+    print("3. Validar Informacion en Celda")
+    print("4. Preview Celda")
+    print("5. Sumar Valores en Fila")
+    print("6. Sumar Valores en Columna")
+    print("7. Exit")
+    print("="*20)
+
+def handle_choice(choice, sheet):
+    if choice == '1':
+        print("Insertar Informacion en celda.")
+        sheet.preview()
+        row = input("Elige la fila: ").strip()
+        column = input("Elige la columna: ").strip()
+        value = input("Escribe el valor a insertar: ").strip()
+        sheet.insert(int(row), int(column), int(value))
+    elif choice == '2':
+        print("Actualizar Informacion en celda.")
+        sheet.preview()
+        row = input("Elige la fila: ").strip()
+        column = input("Elige la columna: ").strip()
+        value = input("Escribe el valor a insertar: ").strip()
+        sheet.update(int(row), int(column), int(value))
+    elif choice == '3':
+        print("Validar informacin en celda.")
+        row = input("Elige la fila: ").strip()
+        column = input("Elige la columna: ").strip()
+        print(f"Celda ({row},{column}) has value: {"contiene informacion" if sheet.has_value(int(row), int(column)) else "no contiene informacion"}")
+    elif choice == '4':
+        print("Preview.")
+        sheet.preview()
+    elif choice == '5':
+        print("Sumar valores en fila.")
+        sheet.preview()
+        choice = input("Elige la file: ").strip()
+        print(sheet.sum_row(int(choice)))
+    elif choice == '6':
+        print("Sumar valores en columna.")
+        sheet.preview()
+        choice = input("Elige la columna: ").strip()
+        print(sheet.sum_column(int(choice)))
+    elif choice == '7':
+        print("Goodbye!")
+        sys.exit()
+    else:
+        print("Invalid choice. Please try again.")
+
+    input("\nPress Enter to continue...") # Pauses the script
+
+def main():
+    sheet = ExcelSheet(rows=5, cols=5)
+    while True:
+        display_menu()
+        choice = input("Elige una opcion (1-7): ").strip()
+        handle_choice(choice, sheet)
+
+if __name__ == "__main__":
+    main()
